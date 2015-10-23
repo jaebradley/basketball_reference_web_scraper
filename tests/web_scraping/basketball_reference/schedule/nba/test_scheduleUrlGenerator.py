694ada56792a049b9d9b14f74de2c90ddf6a8ba7
jaebradley <jae.b.bradley@gmail.com>
2015-10-23 12:16:42 -0400

    adding schedule url generator test

---
+++ tests/web_scraping/basketball_reference/schedule/nba/test_scheduleUrlGenerator.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from src.web_scraping.basketball_reference.schedule.nba.schedule_url_generator import ScheduleUrlGenerator
+
+
+class TestScheduleUrlGenerator(TestCase):
+    def test_expected(self):
+        test_year = 2014
+        result = ScheduleUrlGenerator.generate_url(test_year)
+        expected_result = "http://www.basketball-reference.com/leagues/NBA_{0}_games.html".format(test_year)
+        assert expected_result == result
+
+    def test_assertions(self):
+
+        self.assertRaises(AssertionError, ScheduleUrlGenerator.generate_url, None)
+        self.assertRaises(AssertionError, ScheduleUrlGenerator.generate_url, "Foo")
