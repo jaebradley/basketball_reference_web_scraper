729984ed4d614ef61ab214fee5950152fbc98609
Jae Bradley <jae.b.bradley@gmail.com>
2018-10-24 23:43:22 -0700

    rename broken test

--- test_get_schedule.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-
-from client import get_schedule
-
-
-class TestGet_schedule(TestCase):
-    def test_get_schedule(self):
-        result = get_schedule(2018)
-        self.assertIsNotNone(result)
---
+++ tests/test_get_season_schedule.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+from client import get_season_schedule
+
+
+class TestGet_season_schedule(TestCase):
+    def test_get_season_schedule(self):
+        result = get_season_schedule(2018)
+        self.assertIsNotNone(result)
