20b64239ed05143f2b331baf6ac935d294900562
Jae Bradley <jae.b.bradley@gmail.com>
2018-10-25 22:12:24 -0700

    Implement JSON output for box scores

--- client.py
+++ client.py
@@ -1,52 +1,16 @@
-import requests
+import json
 
-from errors import InvalidDate
-from parsers.box_scores import parse_box_score
-from parsers.schedule import parse_schedule, parse_schedule_for_month_url_paths
-
-BASE_URL = 'http://www.basketball-reference.com'
+from http_client import get_box_scores
+from data import OutputType
+from errors import UnknownOutputType
 
 
-def get_box_scores(day, month, year):
-    url = '{BASE_URL}/friv/dailyleaders.cgi?month={month}&day={day}&year={year}'.format(
-        BASE_URL=BASE_URL,
-        day=day,
-        month=month,
-        year=year
-    )
+def box_scores(day, month, year, output_type=None):
+    values = get_box_scores(day=day, month=month, year=year)
+    if output_type is None:
+        return values
 
-    response = requests.get(url=url, allow_redirects=False)
+    if output_type == OutputType.JSON:
+        return json.dumps(values, sort_keys=True, indent=4)
 
-    if 200 <= response.status_code < 300:
-        return parse_box_score(response.content)
-
-    raise InvalidDate(day=day, month=month, year=year)
-
-
-def get_schedule_for_month(url):
-    response = requests.get(url=url)
-
-    response.raise_for_status()
-
-    return parse_schedule(response.content)
-
-
-def get_season_schedule(season_end_year):
-    url = '{BASE_URL}/leagues/NBA_{season_end_year}_games.html'.format(
-        BASE_URL=BASE_URL,
-        season_end_year=season_end_year
-    )
-
-    response = requests.get(url=url)
-
-    response.raise_for_status()
-
-    season_schedule = parse_schedule(response.content)
-    other_month_url_paths = parse_schedule_for_month_url_paths(response.content)
-
-    for month_url_path in other_month_url_paths:
-        url = '{BASE_URL}{month_url_path}'.format(BASE_URL=BASE_URL, month_url_path=month_url_path)
-        schedule_for_month = get_schedule_for_month(url=url)
-        season_schedule.extend(schedule_for_month)
-
-    return season_schedule
+    raise UnknownOutputType(output_type)
