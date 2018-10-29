b9622e2ff74b04096a803da1bb339a56dfb7c19c
Jae Bradley <jae.b.bradley@gmail.com>
2018-10-28 23:18:48 -0700

    removed unnecessary files and fixed some existing tests

--- models/schedule.py
+++
@@ -1,5 +0,0 @@
-class Schedule:
-    def __init__(self, parsed_event_list, start_year, end_year):
-        self.end_year = end_year
-        self.start_year = start_year
-        self.parsed_event_list = parsed_event_list
--- tests/test_season_schedule.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-
-from basketball_reference_web_scraper.http_client import season_schedule
-
-
-class TestGet_season_schedule(TestCase):
-    def test_get_season_schedule(self):
-        result = season_schedule(2018)
-        self.assertIsNotNone(result)
