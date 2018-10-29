8c1117164b504c7b523fb41868c0237176dbc9f9
Jae Bradley <jae.b.bradley@gmail.com>
2018-10-28 23:48:59 -0700

    add Python 3 requirement

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/jaebradley/basketball_reference_web_scraper",
     packages=setuptools.find_packages(exclude=["tests"]),
+    python_requires=">=3.0.0",
     install_requires=[
         "certifi==2018.10.15",
         "chardet==3.0.4",
