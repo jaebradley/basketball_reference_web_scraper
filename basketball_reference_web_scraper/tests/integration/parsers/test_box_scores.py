206ccd9dd78ada64122fc31d61b5b841fb2e345a
Jae Bradley <jae.b.bradley@gmail.com>
2018-10-28 18:39:51 -0700

    add box score integration test

--- basketball_reference_web_scraper/tests/integration/parsers/test_box_scores.py
+++ basketball_reference_web_scraper/tests/integration/parsers/test_box_scores.py
@@ -1,48 +1,44 @@
 from unittest import TestCase
 
-from basketball_reference_web_scraper.data import Outcome, Location
+from basketball_reference_web_scraper.data import Outcome, Location, Team
 from basketball_reference_web_scraper.parsers import box_scores
 
 
-class TestSchedule(TestCase):
-    def test_parse_seconds_played_for_empty_string(self):
-        self.assertEqual(0, box_scores.parse_seconds_played(""))
+class TestBoxScores(TestCase):
+    def setUp(self):
+        with open('./12_18_2015_daily_leaders.html', 'r') as december_18_2015_daily_leaders_html:
+            self.december_18_2015_daily_leaders = december_18_2015_daily_leaders_html.read()
 
-    def test_parse_seconds_played_for_0_seconds(self):
-        self.assertEqual(1, box_scores.parse_seconds_played("0:01"))
+        with open('./01_29_2017_daily_leaders.html', 'r') as january_01_2017_daily_leaders_html:
+            self.january_01_2017_daily_leaders = january_01_2017_daily_leaders_html.read()
 
-    def test_parse_seconds_played_for_59_seconds(self):
-        self.assertEqual(59, box_scores.parse_seconds_played("0:59"))
+    def test_box_scores_for_12_18_2015(self):
+        parsed_box_score = box_scores.parse_box_score(self.december_18_2015_daily_leaders)
+        self.assertEqual(len(parsed_box_score), 250)
 
-    def test_parse_seconds_played_for_60_seconds(self):
-        self.assertEqual(60, box_scores.parse_seconds_played("1:00"))
+    # Test for minutes played greater than or equal to 60 minutes
+    def test_box_scores_for_01_01_2017(self):
+        parsed_box_score = box_scores.parse_box_score(self.january_01_2017_daily_leaders)
+        self.assertEqual(len(parsed_box_score), 170)
 
-    def test_parse_seconds_played_for_61_seconds(self):
-        self.assertEqual(61, box_scores.parse_seconds_played("1:01"))
+        first_box_score = parsed_box_score[0]
 
-    def test_parse_seconds_played_for_59_minutes_59_seconds(self):
-        self.assertEqual(3599, box_scores.parse_seconds_played("59:59"))
-
-    def test_parse_seconds_played_for_60_minutes(self):
-        self.assertEqual(3600, box_scores.parse_seconds_played("60:00"))
-
-    def test_parse_seconds_played_for_60_minutes_and_1_second(self):
-        self.assertEqual(3601, box_scores.parse_seconds_played("60:01"))
-
-    def test_parse_win(self):
-        self.assertEqual(Outcome.WIN, box_scores.parse_outcome("W"))
-
-    def test_parse_loss(self):
-        self.assertEqual(Outcome.LOSS, box_scores.parse_outcome("L"))
-
-    def test_parse_unknown_outcome_symbol(self):
-        self.assertRaises(ValueError, box_scores.parse_outcome, "jaebaebae")
-
-    def test_parse_away_symbol(self):
-        self.assertEqual(Location.AWAY, box_scores.parse_location("@"))
-
-    def test_parse_home_symbol(self):
-        self.assertEqual(Location.HOME, box_scores.parse_location(""))
-
-    def test_parse_unknown_location_symbol(self):
-        self.assertRaises(ValueError, box_scores.parse_location, "jaebaebae")
+        self.assertEqual(first_box_score["name"], "Paul Millsap")
+        self.assertEqual(first_box_score["team"], Team.ATLANTA_HAWKS)
+        self.assertEqual(first_box_score["opponent"], Team.NEW_YORK_KNICKS)
+        self.assertEqual(first_box_score["outcome"], Outcome.WIN)
+        self.assertEqual(first_box_score["seconds_played"], 3607)
+        self.assertEqual(first_box_score["made_field_goals"], 13)
+        self.assertEqual(first_box_score["attempted_field_goals"], 29)
+        self.assertEqual(first_box_score["made_three_point_field_goals"], 3)
+        self.assertEqual(first_box_score["attempted_three_point_field_goals"], 8)
+        self.assertEqual(first_box_score["made_free_throws"], 8)
+        self.assertEqual(first_box_score["attempted_free_throws"], 10)
+        self.assertEqual(first_box_score["offensive_rebounds"], 8)
+        self.assertEqual(first_box_score["defensive_rebounds"], 11)
+        self.assertEqual(first_box_score["assists"], 7)
+        self.assertEqual(first_box_score["steals"], 1)
+        self.assertEqual(first_box_score["blocks"], 0)
+        self.assertEqual(first_box_score["turnovers"], 3)
+        self.assertEqual(first_box_score["personal_fouls"], 4)
+        self.assertEqual(first_box_score["game_score"], 31.3)
