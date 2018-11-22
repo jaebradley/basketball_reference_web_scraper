55abea886d3a865a381f9104a8352db3e3f43fd4
Jae Bradley <jae.b.bradley@gmail.com>
2018-11-22 10:22:14 -0800

    Raise InvalidSeason error when season schedule response status code is 404 (#49)
    Resolves #50

--- tests/test_season_schedule.py
+++ tests/test_season_schedule.py
@@ -1,10 +1,12 @@
 from unittest import TestCase
+from datetime import date
 
+from basketball_reference_web_scraper.errors import InvalidSeason
 from basketball_reference_web_scraper.client import season_schedule
 from basketball_reference_web_scraper.data import OutputType, OutputWriteOption
 
 
-class TestSeason_schedule(TestCase):
+class TestSeasonSchedule(TestCase):
     def test_season_schedule(self):
         result = season_schedule(season_end_year=2018)
         self.assertIsNotNone(result)
@@ -21,3 +23,9 @@ class TestSeason_schedule(TestCase):
 
     def test_2017_season_schedule_csv_append(self):
         season_schedule(season_end_year=2001, output_type=OutputType.CSV, output_file_path="./foo.csv", output_write_option=OutputWriteOption.WRITE)
+
+    def test_future_season_schedule_throws_invalid_season_error(self):
+        current_year = date.today().year
+        future_year = current_year + 10
+        expected_message = "Season end year of {future_year} is invalid".format(future_year=future_year)
+        self.assertRaisesRegexp(InvalidSeason, expected_message, season_schedule, season_end_year=future_year)
