f884428d7ff937d6eef9ea83e255f5ccc4443bd7
Jae Bradley <jae.b.bradley@gmail.com>
2018-11-22 21:16:30 -0800

    Add unit tests for internal server error for season_schedule method (#59)
    Add unit tests for logic added in #49

--- tests/test_season_schedule.py
+++ tests/test_season_schedule.py
@@ -1,9 +1,11 @@
-from unittest import TestCase
 from datetime import date
+from unittest import TestCase, mock
 
-from basketball_reference_web_scraper.errors import InvalidSeason
+from requests import HTTPError, codes
+
 from basketball_reference_web_scraper.client import season_schedule
 from basketball_reference_web_scraper.data import OutputType, OutputWriteOption
+from basketball_reference_web_scraper.errors import InvalidSeason
 
 
 class TestSeasonSchedule(TestCase):
@@ -28,4 +30,17 @@ class TestSeasonSchedule(TestCase):
         current_year = date.today().year
         future_year = current_year + 10
         expected_message = "Season end year of {future_year} is invalid".format(future_year=future_year)
-        self.assertRaisesRegexp(InvalidSeason, expected_message, season_schedule, season_end_year=future_year)
+        self.assertRaisesRegex(InvalidSeason, expected_message, season_schedule, season_end_year=future_year)
+
+    @mock.patch("basketball_reference_web_scraper.client.http_client")
+    def test_not_found_raises_invalid_season(self, mocked_http_client):
+        end_year = "jaebaebae"
+        expected_message = "Season end year of {end_year} is invalid".format(end_year=end_year)
+        mocked_http_client.season_schedule.side_effect = HTTPError(response=mock.Mock(status_code=codes.not_found))
+        self.assertRaisesRegex(InvalidSeason, expected_message, season_schedule, season_end_year=end_year)
+
+    @mock.patch("basketball_reference_web_scraper.client.http_client")
+    def test_other_http_error_is_raised(self, mocked_http_client):
+        mocked_http_client.season_schedule.side_effect = HTTPError(
+            response=mock.Mock(status_code=codes.internal_server_error))
+        self.assertRaises(HTTPError, season_schedule, season_end_year=2018)
