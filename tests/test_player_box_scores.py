a6272b8886a5dddef7d928c0b048b6552a94fded
Jae Bradley <jae.b.bradley@gmail.com>
2018-11-23 23:10:18 -0800

    Raise invalid date for non-200 player box score responses

--- tests/test_player_box_scores.py
+++ tests/test_player_box_scores.py
@@ -1,4 +1,6 @@
-from unittest import TestCase
+from unittest import TestCase, mock
+
+from requests import HTTPError, codes
 
 from basketball_reference_web_scraper.client import player_box_scores
 from basketball_reference_web_scraper.data import OutputType, OutputWriteOption
@@ -11,7 +13,25 @@ class TestPlayerBoxScores(TestCase):
         self.assertIsNotNone(result)
 
     def test_get_box_scores_for_day_that_does_not_exist(self):
-        self.assertRaises(InvalidDate, player_box_scores, day=-1, month=1, year=2018)
+        self.assertRaisesRegex(
+            InvalidDate,
+            "Date with year set to 2018, month set to 1, and day set to -1 is invalid",
+            player_box_scores,
+            day=-1, month=1, year=2018)
 
     def test_get_box_scores_from_2001(self):
-        player_box_scores(day=1, month=1, year=2001, output_type=OutputType.CSV, output_file_path="./foo.csv", output_write_option=OutputWriteOption.WRITE)
+        player_box_scores(
+            day=1, month=1, year=2001,
+            output_type=OutputType.CSV, output_file_path="./foo.csv", output_write_option=OutputWriteOption.WRITE)
+
+    def test_raises_invalid_date_for_nonexistent_dates(self):
+        self.assertRaisesRegex(
+            InvalidDate,
+            "Date with year set to baz, month set to bar, and day set to foo is invalid",
+            player_box_scores,
+            day="foo", month="bar", year="baz")
+
+    @mock.patch("basketball_reference_web_scraper.client.http_client")
+    def test_raises_invalid_date_for_404_response(self, mocked_http_client):
+        mocked_http_client.player_box_scores.side_effect = HTTPError(response=mock.Mock(status_code=codes.not_found))
+        self.assertRaises(InvalidDate, player_box_scores, day=1, month=1, year=2018)
