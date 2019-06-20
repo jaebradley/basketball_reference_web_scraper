3ff42f0fa9632a1e9a6c1bdd0eb1f9001e53aa75
DaiJunyan <daiju@kean.edu>
2019-06-20 10:26:55 +0800

    add a new method players_season_advanced (#84)
    Added a new method players_season_advanced and updated the README.md

--- basketball_reference_web_scraper/output.py
+++ basketball_reference_web_scraper/output.py
@@ -59,6 +59,36 @@ player_season_totals_fieldname = [
     "personal_fouls",
 ]
 
+player_season_advanced_fieldname = [
+    "slug",
+    "name",
+    "positions",
+    "age",
+    "team",
+    "games_played",
+    "minutes_played",
+    "player_efficiency_rating",
+    "true_shooting_percentage",
+    "three_point_attempt_rate",
+    "free_throw_attempt_rate",
+    "offensive_rebound_percentage",
+    "defensive_rebound_percentage",
+    "total_rebound_percentage",
+    "assist_percentage",
+    "steal_percentage",
+    "block_percentage",
+    "turnover_percentage",
+    "usage_percentage",
+    "offensive_win_shares",
+    "defensive_win_shares",
+    "win_shares",
+    "win_shares_per_48_minutes",
+    "offensive_box_plus_minus",
+    "defensive_box_plus_minus",
+    "box_plus_minus",
+    "value_over_replacement_player",
+]
+
 team_box_score_fieldname = [
     "team",
     "minutes_played",
@@ -188,6 +218,43 @@ def players_season_totals_to_csv(rows, output_file_path, write_option):
                 "blocks": row["blocks"],
                 "turnovers": row["turnovers"],
                 "personal_fouls": row["personal_fouls"],
+            } for row in rows
+        )
+
+
+def players_season_advanced_to_csv(rows, output_file_path, write_option):
+    with open(output_file_path, write_option.value, newline="") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=player_season_advanced_fieldname)
+        writer.writeheader()
+        writer.writerows(
+            {
+                "slug": row["slug"],
+                "name": row["name"],
+                "positions": "-".join(map(lambda position: position.value, row["positions"])),
+                "age": row["age"],
+                "team": row["team"].value,
+                "games_played": row["games_played"],
+                "minutes_played": row["minutes_played"],
+                "player_efficiency_rating": row["player_efficiency_rating"],
+                "true_shooting_percentage": row["true_shooting_percentage"],
+                "three_point_attempt_rate": row["three_point_attempt_rate"],
+                "free_throw_attempt_rate": row["free_throw_attempt_rate"],
+                "offensive_rebound_percentage": row["offensive_rebound_percentage"],
+                "defensive_rebound_percentage": row["defensive_rebound_percentage"],
+                "total_rebound_percentage": row["total_rebound_percentage"],
+                "assist_percentage": row["assist_percentage"],
+                "steal_percentage": row["steal_percentage"],
+                "block_percentage": row["block_percentage"],
+                "turnover_percentage": row["turnover_percentage"],
+                "usage_percentage": row["usage_percentage"],
+                "offensive_win_shares": row["offensive_win_shares"],
+                "defensive_win_shares": row["defensive_win_shares"],
+                "win_shares": row["win_shares"],
+                "win_shares_per_48_minutes": row["win_shares_per_48_minutes"],
+                "offensive_box_plus_minus": row["offensive_box_plus_minus"],
+                "defensive_box_plus_minus": row["defensive_box_plus_minus"],
+                "box_plus_minus": row["box_plus_minus"],
+                "value_over_replacement_player": row["value_over_replacement_player"],
             } for row in rows
         )
 
