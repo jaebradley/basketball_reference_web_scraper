0632374867f41484a7b4780febf1c7da5646db22
Jae Bradley <jae.b.bradley@gmail.com>
2019-10-25 20:08:08 -0700

    Refactor player box scores parser (#105)

--- basketball_reference_web_scraper/html.py
+++ basketball_reference_web_scraper/html.py
@@ -1,5 +1,6 @@
 import re
 
+
 class PlayerSeasonTotalTable:
     def __init__(self, html):
         self.html = html
@@ -229,3 +230,104 @@ class TeamTotalRow:
     @property
     def personal_fouls(self):
         return self.html[17].text_content()
+
+
+class DailyLeadersPage:
+    def __init__(self, html):
+        self.html = html
+
+    @property
+    def daily_leaders(self):
+        return [
+            PlayerBoxScoreRow(row_html)
+            for row_html in self.html.xpath('//table[@id="stats"]//tbody/tr[not(contains(@class, "thead"))]')
+        ]
+
+
+class PlayerBoxScoreRow:
+    def __init__(self, html):
+        self.html = html
+
+    @property
+    def slug(self):
+        return self.html[1].get('data-append-csv')
+
+    @property
+    def name(self):
+        return self.html[1].text_content()
+
+    @property
+    def team_abbreviation(self):
+        return self.html[2].text_content()
+
+    @property
+    def location_abbreviation(self):
+        return self.html[3].text_content()
+
+    @property
+    def opponent_abbreviation(self):
+        return self.html[4].text_content()
+
+    @property
+    def outcome(self):
+        return self.html[5].text_content()
+
+    @property
+    def playing_time(self):
+        return self.html[6].text_content()
+
+    @property
+    def made_field_goals(self):
+        return self.html[7].text_content()
+
+    @property
+    def attempted_field_goals(self):
+        return self.html[8].text_content()
+
+    @property
+    def made_three_point_field_goals(self):
+        return self.html[10].text_content()
+
+    @property
+    def attempted_three_point_field_goals(self):
+        return self.html[11].text_content()
+
+    @property
+    def made_free_throws(self):
+        return self.html[13].text_content()
+
+    @property
+    def attempted_free_throws(self):
+        return self.html[14].text_content()
+
+    @property
+    def offensive_rebounds(self):
+        return self.html[16].text_content()
+
+    @property
+    def defensive_rebounds(self):
+        return self.html[17].text_content()
+
+    @property
+    def assists(self):
+        return self.html[19].text_content()
+
+    @property
+    def steals(self):
+        return self.html[20].text_content()
+
+    @property
+    def blocks(self):
+        return self.html[21].text_content()
+
+    @property
+    def turnovers(self):
+        return self.html[22].text_content()
+
+    @property
+    def personal_fouls(self):
+        return self.html[23].text_content()
+
+    @property
+    def game_score(self):
+        return self.html[25].text_content()
