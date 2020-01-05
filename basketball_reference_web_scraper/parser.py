d771755550c6c629981b40f7135b2f0e82a011d1
Jae Bradley <jae.b.bradley@gmail.com>
2020-01-05 00:58:35 -0500

    Refactor player advanced season totals parser to use object-oriented approach

--- basketball_reference_web_scraper/parser.py
+++ basketball_reference_web_scraper/parser.py
@@ -67,6 +67,45 @@ class SecondsPlayedParser:
         return 60 * int(minutes_played) + int(seconds_played)
 
 
+class PlayerAdvancedSeasonTotalsParser:
+    def __init__(self, position_abbreviation_parser, team_abbreviation_parser):
+        self.position_abbreviation_parser = position_abbreviation_parser
+        self.team_abbreviation_parser = team_abbreviation_parser
+
+    def parse(self, totals):
+        return [
+            {
+                "slug": str(total.slug),
+                "name": str(total.name),
+                "positions": self.position_abbreviation_parser.from_abbreviations(total.position_abbreviations),
+                "age": str_to_int(total.age, default=None),
+                "team": self.team_abbreviation_parser.from_abbreviation(total.team_abbreviation),
+                "games_played": str_to_int(total.games_played),
+                "minutes_played": str_to_int(total.minutes_played),
+                "player_efficiency_rating": str_to_float(total.player_efficiency_rating),
+                "true_shooting_percentage": str_to_float(total.true_shooting_percentage),
+                "three_point_attempt_rate": str_to_float(total.three_point_attempt_rate),
+                "free_throw_attempt_rate": str_to_float(total.free_throw_attempt_rate),
+                "offensive_rebound_percentage": str_to_float(total.offensive_rebound_percentage),
+                "defensive_rebound_percentage": str_to_float(total.defensive_rebound_percentage),
+                "total_rebound_percentage": str_to_float(total.total_rebound_percentage),
+                "assist_percentage": str_to_float(total.assist_percentage),
+                "steal_percentage": str_to_float(total.steal_percentage),
+                "block_percentage": str_to_float(total.block_percentage),
+                "turnover_percentage": str_to_float(total.turnover_percentage),
+                "usage_percentage": str_to_float(total.usage_percentage),
+                "offensive_win_shares": str_to_float(total.offensive_win_shares),
+                "defensive_win_shares": str_to_float(total.defensive_win_shares),
+                "win_shares": str_to_float(total.win_shares),
+                "win_shares_per_48_minutes": str_to_float(total.win_shares_per_48_minutes),
+                "offensive_box_plus_minus": str_to_float(total.offensive_plus_minus),
+                "defensive_box_plus_minus": str_to_float(total.defensive_plus_minus),
+                "box_plus_minus": str_to_float(total.plus_minus),
+                "value_over_replacement_player": str_to_float(total.value_over_replacement_player),
+            } for total in totals
+        ]
+
+
 class PlayerSeasonTotalsParser:
     def __init__(self, position_abbreviation_parser, team_abbreviation_parser):
         self.position_abbreviation_parser = position_abbreviation_parser
