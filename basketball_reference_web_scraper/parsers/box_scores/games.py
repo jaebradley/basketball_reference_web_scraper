020c151d9013594c23f82bd176ddf9e28eef7924
Jae Bradley <jae.b.bradley@gmail.com>
2020-01-21 07:49:50 -0500

    Remove unused game parsing logic (#118)
    Remove all files in the `parsers` directory.

--- basketball_reference_web_scraper/parsers/box_scores/games.py
+++
@@ -1,7 +0,0 @@
-from lxml import html
-
-
-def parse_game_url_paths(page):
-    tree = html.fromstring(page)
-    games = tree.xpath('//td[contains(@class, "gamelink")]/a')
-    return list(map(lambda game: game.attrib['href'], games))
