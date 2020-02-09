ce49758061b1de7b86c59deb389822aa916d5b1a
Jae Bradley <jae.b.bradley@gmail.com>
2020-02-09 10:02:53 -0500

    Test PlayerPageTotalsRow HTML Class

---
+++ tests/test_player_page_totals_row.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+from unittest.mock import MagicMock
+
+from basketball_reference_web_scraper.html import PlayerPageTotalsRow
+
+
+class TestPlayerPageTotalsRow(TestCase):
+    def test_league_abbreviation_is_none_when_no_matching_league_abbreviations(self):
+        html = MagicMock()
+        html.xpath = MagicMock(return_value=[])
+
+        self.assertIsNone(PlayerPageTotalsRow(html=html).league_abbreviation)
+        html.xpath.assert_called_once_with('.//td[@data-stat="lg_id"]')
+
+    def test_league_abbreviation_is_first_abbreviation_text_content_when_matching_league_abbreviations(self):
+        first_abbreviation = MagicMock()
+        first_abbreviation.text_content = MagicMock(return_value="first abbreviation")
+
+        second_abbreviation = MagicMock()
+        second_abbreviation.text_content = MagicMock(return_value="second abbreviation")
+
+        html = MagicMock()
+        html.xpath = MagicMock(return_value=[first_abbreviation, second_abbreviation])
+
+        self.assertEqual(
+            PlayerPageTotalsRow(html=html).league_abbreviation,
+            "first abbreviation"
+        )
+        html.xpath.assert_called_once_with('.//td[@data-stat="lg_id"]')
+
