55b53a0a65f1fe615151e1a1d808dd07b40b8073
Jae Bradley <jae.b.bradley@gmail.com>
2020-02-25 18:25:41 -0500

    Update tests for player advanced season totals

--- tests/integration/test_players_advanced_season_totals.py
+++ tests/integration/test_players_advanced_season_totals.py
@@ -1,15 +1,69 @@
-from datetime import date
-from unittest import TestCase, mock
 import os
-
-from requests import HTTPError, codes
+from datetime import date
+from unittest import TestCase
 
 from basketball_reference_web_scraper.client import players_advanced_season_totals
-from basketball_reference_web_scraper.data import OutputType, OutputWriteOption
+from basketball_reference_web_scraper.data import OutputType
 from basketball_reference_web_scraper.errors import InvalidSeason
 
 
-class TestPlayerAdvancedSeasonTotals(TestCase):
+class BaseTestPlayerAdvancedSeasonTotalsCSVOutput(TestCase):
+    @property
+    def year(self):
+        raise NotImplementedError
+
+    def setUp(self):
+        self.output_file_path = os.path.join(
+            os.path.dirname(__file__),
+            "./output/player_advanced_season_totals_{year}.csv".format(year=self.year),
+        )
+        self.expected_output_file_path = os.path.join(
+            os.path.dirname(__file__),
+            "./output/expected/player_advanced_season_totals_{year}.csv".format(year=self.year),
+        )
+
+    def tearDown(self):
+        os.remove(self.output_file_path)
+
+    def assert_player_advanced_season_totals_csv(self):
+        players_advanced_season_totals(
+            season_end_year=self.year,
+            output_type=OutputType.CSV,
+            output_file_path=self.output_file_path,
+        )
+
+        with open(self.output_file_path, "r") as output_file, \
+                open(self.expected_output_file_path, "r") as expected_output_file:
+            self.assertEqual(
+                output_file.readlines(),
+                expected_output_file.readlines(),
+            )
+
+
+class Test2018PlayerAdvancedSeasonTotalsCSVOutput(BaseTestPlayerAdvancedSeasonTotalsCSVOutput):
+    @property
+    def year(self):
+        return 2018
+
+    def test_players_advanced_season_totals_csv(self):
+        self.assert_player_advanced_season_totals_csv()
+
+
+class Test2001PlayerAdvancedSeasonTotalsCSVOutput(BaseTestPlayerAdvancedSeasonTotalsCSVOutput):
+    @property
+    def year(self):
+        return 2001
+
+    def test_players_advanced_season_totals_csv(self):
+        self.assert_player_advanced_season_totals_csv()
+
+
+class TestPlayerAdvancedSeasonTotalsInMemoryOutput(TestCase):
+    def test_future_season_raises_invalid_season(self):
+        current_year = date.today().year
+        future_year = current_year + 10
+        expected_message = "Season end year of {future_year} is invalid".format(future_year=future_year)
+        self.assertRaisesRegex(InvalidSeason, expected_message, players_advanced_season_totals, season_end_year=future_year)
 
     def test_players_advanced_season_totals(self):
         result = players_advanced_season_totals(season_end_year=2018)
@@ -18,56 +72,3 @@ class TestPlayerAdvancedSeasonTotals(TestCase):
     def test_players_advanced_season_totals_json(self):
         result = players_advanced_season_totals(season_end_year=2018, output_type=OutputType.JSON)
         self.assertIsNotNone(result)
-
-    def test_players_advanced_season_totals_csv(self):
-        output_file_path = os.path.join(
-            os.path.dirname(__file__),
-            "./output/player_advanced_season_totals_2019.csv",
-        )
-        players_advanced_season_totals(
-            season_end_year=2018,
-            output_type=OutputType.CSV,
-            output_file_path=output_file_path,
-        )
-
-    def test_players_advanced_season_totals_csv_append(self):
-        output_file_path = os.path.join(
-            os.path.dirname(__file__),
-            "./output/player_advanced_season_totals_2019.csv",
-        )
-        players_advanced_season_totals(
-            season_end_year=2018,
-            output_type=OutputType.CSV,
-            output_file_path=output_file_path,
-            output_write_option=OutputWriteOption.APPEND,
-        )
-
-    def test_2001_players_advanced_season_totals_csv(self):
-        output_file_path = os.path.join(
-            os.path.dirname(__file__),
-            "./output/player_advanced_season_totals_2001.csv",
-        )
-        players_advanced_season_totals(
-            season_end_year=2001,
-            output_type=OutputType.CSV,
-            output_file_path=output_file_path,
-            output_write_option=OutputWriteOption.WRITE,
-        )
-
-    def test_future_season_raises_invalid_season(self):
-        current_year = date.today().year
-        future_year = current_year + 10
-        expected_message = "Season end year of {future_year} is invalid".format(future_year=future_year)
-        self.assertRaisesRegex(InvalidSeason, expected_message, players_advanced_season_totals, season_end_year=future_year)
-
-    @mock.patch("basketball_reference_web_scraper.client.http_client")
-    def test_not_found_raises_invalid_season(self, mocked_http_client):
-        end_year = "jaebaebae"
-        expected_message = "Season end year of {end_year} is invalid".format(end_year=end_year)
-        mocked_http_client.players_advanced_season_totals.side_effect = HTTPError(response=mock.Mock(status_code=codes.not_found))
-        self.assertRaisesRegex(InvalidSeason, expected_message, players_advanced_season_totals, season_end_year=end_year)
-
-    @mock.patch("basketball_reference_web_scraper.client.http_client")
-    def test_other_http_error_is_raised(self, mocked_http_client):
-        mocked_http_client.players_advanced_season_totals.side_effect = HTTPError(response=mock.Mock(status_code=codes.internal_server_error))
-        self.assertRaises(HTTPError, players_advanced_season_totals, season_end_year=2018)
