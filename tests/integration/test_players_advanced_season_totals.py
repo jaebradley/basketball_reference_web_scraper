a8679b0a2532672eddb983c11984883d54dbf145
Jae Bradley <jae.b.bradley@gmail.com>
2020-02-26 09:35:59 -0500

    Update player advanced season totals integration tests (#162)

--- tests/integration/test_players_advanced_season_totals.py
+++ tests/integration/test_players_advanced_season_totals.py
@@ -1,73 +1,208 @@
-from datetime import date
-from unittest import TestCase, mock
+import json
 import os
-
-from requests import HTTPError, codes
+from datetime import date
+from unittest import TestCase
 
 from basketball_reference_web_scraper.client import players_advanced_season_totals
-from basketball_reference_web_scraper.data import OutputType, OutputWriteOption
+from basketball_reference_web_scraper.data import OutputType, Team, Position
 from basketball_reference_web_scraper.errors import InvalidSeason
 
 
-class TestPlayerAdvancedSeasonTotals(TestCase):
+class BaseTestPlayerAdvancedSeasonTotalsCSVOutput(TestCase):
+    @property
+    def year(self):
+        raise NotImplementedError
 
-    def test_players_advanced_season_totals(self):
-        result = players_advanced_season_totals(season_end_year=2018)
-        self.assertIsNotNone(result)
+    def setUp(self):
+        self.output_file_path = os.path.join(
+            os.path.dirname(__file__),
+            "./output/player_advanced_season_totals_{year}.csv".format(year=self.year),
+        )
+        self.expected_output_file_path = os.path.join(
+            os.path.dirname(__file__),
+            "./output/expected/player_advanced_season_totals_{year}.csv".format(year=self.year),
+        )
 
-    def test_players_advanced_season_totals_json(self):
-        result = players_advanced_season_totals(season_end_year=2018, output_type=OutputType.JSON)
-        self.assertIsNotNone(result)
+    def tearDown(self):
+        os.remove(self.output_file_path)
+
+    def assert_player_advanced_season_totals_csv(self):
+        players_advanced_season_totals(
+            season_end_year=self.year,
+            output_type=OutputType.CSV,
+            output_file_path=self.output_file_path,
+        )
+
+        with open(self.output_file_path, "r") as output_file, \
+                open(self.expected_output_file_path, "r") as expected_output_file:
+            self.assertEqual(
+                output_file.readlines(),
+                expected_output_file.readlines(),
+            )
+
+
+class BaseTestPlayerAdvancedSeasonTotalsJSONOutput(TestCase):
+    @property
+    def year(self):
+        raise NotImplementedError
+
+    def setUp(self):
+        self.output_file_path = os.path.join(
+            os.path.dirname(__file__),
+            "./output/player_advanced_season_totals_{year}.json".format(year=self.year),
+        )
+        self.expected_output_file_path = os.path.join(
+            os.path.dirname(__file__),
+            "./output/expected/player_advanced_season_totals_{year}.json".format(year=self.year),
+        )
+
+    def tearDown(self):
+        os.remove(self.output_file_path)
+
+    def assert_player_advanced_season_totals_json(self):
+        players_advanced_season_totals(
+            season_end_year=self.year,
+            output_type=OutputType.JSON,
+            output_file_path=self.output_file_path,
+        )
+
+        with open(self.output_file_path, "r") as output_file, \
+                open(self.expected_output_file_path, "r") as expected_output_file:
+            self.assertEqual(
+                json.load(output_file),
+                json.load(expected_output_file),
+            )
+
+
+class Test2018PlayerAdvancedSeasonTotalsCSVOutput(BaseTestPlayerAdvancedSeasonTotalsCSVOutput):
+    @property
+    def year(self):
+        return 2018
 
     def test_players_advanced_season_totals_csv(self):
-        output_file_path = os.path.join(
-            os.path.dirname(__file__),
-            "./output/player_advanced_season_totals_2019.csv",
-        )
-        players_advanced_season_totals(
-            season_end_year=2018,
-            output_type=OutputType.CSV,
-            output_file_path=output_file_path,
-        )
+        self.assert_player_advanced_season_totals_csv()
 
-    def test_players_advanced_season_totals_csv_append(self):
-        output_file_path = os.path.join(
-            os.path.dirname(__file__),
-            "./output/player_advanced_season_totals_2019.csv",
-        )
-        players_advanced_season_totals(
-            season_end_year=2018,
-            output_type=OutputType.CSV,
-            output_file_path=output_file_path,
-            output_write_option=OutputWriteOption.APPEND,
-        )
 
-    def test_2001_players_advanced_season_totals_csv(self):
-        output_file_path = os.path.join(
-            os.path.dirname(__file__),
-            "./output/player_advanced_season_totals_2001.csv",
-        )
-        players_advanced_season_totals(
-            season_end_year=2001,
-            output_type=OutputType.CSV,
-            output_file_path=output_file_path,
-            output_write_option=OutputWriteOption.WRITE,
-        )
+class Test2001PlayerAdvancedSeasonTotalsCSVOutput(BaseTestPlayerAdvancedSeasonTotalsCSVOutput):
+    @property
+    def year(self):
+        return 2001
 
+    def test_players_advanced_season_totals_csv(self):
+        self.assert_player_advanced_season_totals_csv()
+
+
+class Test2018PlayerAdvancedSeasonTotalsJSONOutput(BaseTestPlayerAdvancedSeasonTotalsJSONOutput):
+    @property
+    def year(self):
+        return 2018
+
+    def test_players_advanced_season_totals_json(self):
+        self.assert_player_advanced_season_totals_json()
+
+
+class Test2001PlayerAdvancedSeasonTotalsJSONOutput(BaseTestPlayerAdvancedSeasonTotalsJSONOutput):
+    @property
+    def year(self):
+        return 2001
+
+    def test_players_advanced_season_totals_json(self):
+        self.assert_player_advanced_season_totals_json()
+
+
+class TestPlayerAdvancedSeasonTotalsInMemoryOutput(TestCase):
     def test_future_season_raises_invalid_season(self):
         current_year = date.today().year
         future_year = current_year + 10
         expected_message = "Season end year of {future_year} is invalid".format(future_year=future_year)
         self.assertRaisesRegex(InvalidSeason, expected_message, players_advanced_season_totals, season_end_year=future_year)
 
-    @mock.patch("basketball_reference_web_scraper.client.http_client")
-    def test_not_found_raises_invalid_season(self, mocked_http_client):
-        end_year = "jaebaebae"
-        expected_message = "Season end year of {end_year} is invalid".format(end_year=end_year)
-        mocked_http_client.players_advanced_season_totals.side_effect = HTTPError(response=mock.Mock(status_code=codes.not_found))
-        self.assertRaisesRegex(InvalidSeason, expected_message, players_advanced_season_totals, season_end_year=end_year)
+    def test_2018_players_advanced_season_totals_length(self):
+        result = players_advanced_season_totals(season_end_year=2018)
+        self.assertEqual(len(result), 605)
 
-    @mock.patch("basketball_reference_web_scraper.client.http_client")
-    def test_other_http_error_is_raised(self, mocked_http_client):
-        mocked_http_client.players_advanced_season_totals.side_effect = HTTPError(response=mock.Mock(status_code=codes.internal_server_error))
-        self.assertRaises(HTTPError, players_advanced_season_totals, season_end_year=2018)
+    def test_first_2018_players_advanced_season_totals_row(self):
+        result = players_advanced_season_totals(season_end_year=2018)
+        self.assertEqual(
+            result[0],
+            {
+                "age": 24,
+                "assist_percentage": 3.4,
+                "block_percentage": 0.6,
+                "box_plus_minus": -1.5,
+                "defensive_box_plus_minus": 0.4,
+                "defensive_rebound_percentage": 8.9,
+                "defensive_win_shares": 1.0,
+                "free_throw_attempt_rate": 0.158,
+                "games_played": 75,
+                "minutes_played": 1134,
+                "name": "\u00c1lex Abrines",
+                "offensive_box_plus_minus": -1.9,
+                "offensive_rebound_percentage": 2.5,
+                "offensive_win_shares": 1.3,
+                "player_efficiency_rating": 9.0,
+                "positions": [
+                    Position.SHOOTING_GUARD,
+                ],
+                "slug": "abrinal01",
+                "steal_percentage": 1.7,
+                "team": Team.OKLAHOMA_CITY_THUNDER,
+                "three_point_attempt_rate": 0.759,
+                "total_rebound_percentage": 5.6,
+                "true_shooting_percentage": 0.567,
+                "turnover_percentage": 7.4,
+                "usage_percentage": 12.7,
+                "value_over_replacement_player": 0.1,
+                "win_shares": 2.2,
+                "win_shares_per_48_minutes": 0.094
+            },
+        )
+
+    def test_last_2018_players_advanced_season_totals_row(self):
+        result = players_advanced_season_totals(season_end_year=2018)
+        self.assertEqual(
+            result[604],
+            {
+                "age": 20,
+                "assist_percentage": 8.8,
+                "block_percentage": 3.0,
+                "box_plus_minus": -2.0,
+                "defensive_box_plus_minus": -0.3,
+                "defensive_rebound_percentage": 20.1,
+                "defensive_win_shares": 0.5,
+                "free_throw_attempt_rate": 0.418,
+                "games_played": 43,
+                "minutes_played": 410,
+                "name": "Ivica Zubac",
+                "offensive_box_plus_minus": -1.8,
+                "offensive_rebound_percentage": 11.8,
+                "offensive_win_shares": 0.5,
+                "player_efficiency_rating": 15.3,
+                "positions": [
+                    Position.CENTER
+                ],
+                "slug": "zubaciv01",
+                "steal_percentage": 0.9,
+                "team": Team.LOS_ANGELES_LAKERS,
+                "three_point_attempt_rate": 0.008,
+                "total_rebound_percentage": 16.0,
+                "true_shooting_percentage": 0.557,
+                "turnover_percentage": 15.3,
+                "usage_percentage": 17.6,
+                "value_over_replacement_player": 0.0,
+                "win_shares": 1.0,
+                "win_shares_per_48_minutes": 0.118
+            }
+        )
+
+    def test_players_advanced_season_totals_json(self):
+        expected_output_file_path = os.path.join(
+            os.path.dirname(__file__),
+            "./output/expected/player_advanced_season_totals_2018.json",
+        )
+        result = players_advanced_season_totals(season_end_year=2018, output_type=OutputType.JSON)
+        with open(expected_output_file_path, "r") as expected_output:
+            self.assertEqual(
+                json.loads(result),
+                json.load(expected_output),
+            )
