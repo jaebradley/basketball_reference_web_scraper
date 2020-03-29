a4e9a5348da96005c8b4f8b8cf6cb9430087ef07
Jae Bradley <jae.b.bradley@gmail.com>
2020-03-29 17:57:32 -0400

    Migrate player season totals method to parser service and http service

--- basketball_reference_web_scraper/parser_service.py
+++ basketball_reference_web_scraper/parser_service.py
@@ -12,9 +12,16 @@ from basketball_reference_web_scraper.parsers import PositionAbbreviationParser,
 class ParserService:
     def __init__(self):
         self.team_abbreviation_parser = TeamAbbreviationParser(abbreviations_to_teams=TEAM_ABBREVIATIONS_TO_TEAM)
-        self.location_abbreviation_parser = LocationAbbreviationParser(abbreviations_to_locations=LOCATION_ABBREVIATIONS_TO_POSITION)
-        self.outcome_abbreviation_parser = OutcomeAbbreviationParser(abbreviations_to_outcomes=OUTCOME_ABBREVIATIONS_TO_OUTCOME)
+        self.location_abbreviation_parser = LocationAbbreviationParser(
+            abbreviations_to_locations=LOCATION_ABBREVIATIONS_TO_POSITION,
+        )
+        self.outcome_abbreviation_parser = OutcomeAbbreviationParser(
+            abbreviations_to_outcomes=OUTCOME_ABBREVIATIONS_TO_OUTCOME,
+        )
         self.outcome_parser = PlayerBoxScoreOutcomeParser(outcome_abbreviation_parser=self.outcome_abbreviation_parser)
+        self.position_abbreviation_parser = PositionAbbreviationParser(
+            abbreviations_to_positions=POSITION_ABBREVIATIONS_TO_POSITION,
+        )
         self.seconds_played_parser = SecondsPlayedParser()
 
         self.player_box_scores_parser = PlayerBoxScoresParser(
@@ -29,9 +36,16 @@ class ParserService:
             outcome_parser=self.outcome_parser,
             seconds_played_parser=self.seconds_played_parser
         )
+        self.player_season_totals_parser = PlayerSeasonTotalsParser(
+            position_abbreviation_parser=self.position_abbreviation_parser,
+            team_abbreviation_parser=self.team_abbreviation_parser,
+        )
 
     def parse_player_box_scores(self, box_scores):
         return self.player_box_scores_parser.parse(box_scores=box_scores)
 
     def parse_player_season_box_scores(self, box_scores):
         return self.player_season_box_scores_parser.parse(box_scores=box_scores)
+
+    def parse_player_season_totals(self, totals):
+        return self.player_season_totals_parser.parse(totals=totals)
