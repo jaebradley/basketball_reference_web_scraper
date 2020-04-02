001ce158618ab51962451b971c3f32b2c9ff1ed0
Jae Bradley <jae.b.bradley@gmail.com>
2020-04-02 16:06:17 -0400

    Fix test

--- tests/unit/client/test_player_season_totals.py
+++ tests/unit/client/test_player_season_totals.py
@@ -1,22 +1,22 @@
 from unittest import TestCase
-from unittest.mock import patch, MagicMock
+from unittest.mock import MagicMock, patch
 
 from requests import HTTPError, codes
 
-
 from basketball_reference_web_scraper.client import players_season_totals
 from basketball_reference_web_scraper.errors import InvalidSeason
+from basketball_reference_web_scraper.http_service import HTTPService
 
 
 class TestPlayerSeasonTotals(TestCase):
-    @patch("basketball_reference_web_scraper.http_client.players_season_totals")
+    @patch.object(HTTPService, 'players_season_totals')
     def test_not_found_raises_invalid_season(self, mocked_players_season_totals):
         end_year = "jaebaebae"
         expected_message = "Season end year of {end_year} is invalid".format(end_year=end_year)
         mocked_players_season_totals.side_effect = HTTPError(response=MagicMock(status_code=codes.not_found))
         self.assertRaisesRegex(InvalidSeason, expected_message, players_season_totals, season_end_year=end_year)
 
-    @patch("basketball_reference_web_scraper.http_client.players_season_totals")
+    @patch.object(HTTPService, 'players_season_totals')
     def test_other_http_error_is_raised(self, mocked_players_season_totals):
         mocked_players_season_totals.side_effect = HTTPError(
             response=MagicMock(status_code=codes.internal_server_error)
