4728550bc9159acbf977719f3bd1b313512c80b1
Jae Bradley <jae.b.bradley@gmail.com>
2020-04-04 22:00:34 -0400

    Migrate play by plays to HTTPService and ParserService

--- basketball_reference_web_scraper/http_service.py
+++ basketball_reference_web_scraper/http_service.py
@@ -1,9 +1,10 @@
 import requests
 from lxml import html
 
+from basketball_reference_web_scraper.data import TEAM_TO_TEAM_ABBREVIATION
 from basketball_reference_web_scraper.errors import InvalidDate, InvalidPlayerAndSeason
 from basketball_reference_web_scraper.html import DailyLeadersPage, PlayerSeasonBoxScoresPage, PlayerSeasonTotalTable, \
-    PlayerAdvancedSeasonTotalsTable
+    PlayerAdvancedSeasonTotalsTable, PlayByPlayPage
 
 
 class HTTPService:
@@ -50,6 +51,25 @@ class HTTPService:
             raise InvalidPlayerAndSeason(player_identifier=player_identifier, season_end_year=season_end_year)
 
         return self.parser.parse_player_season_box_scores(box_scores=page.regular_season_box_scores_table.rows)
+
+    def play_by_play(self, home_team, day, month, year):
+        add_0_if_needed = lambda s: "0" + s if len(s) == 1 else s
+
+        # the hard-coded `0` in the url assumes we always take the first match of the given date and team.
+        url = "{BASE_URL}/boxscores/pbp/{year}{month}{day}0{team_abbr}.html".format(
+            BASE_URL=HTTPService.BASE_URL, year=year, month=add_0_if_needed(str(month)), day=add_0_if_needed(str(day)),
+            team_abbr=TEAM_TO_TEAM_ABBREVIATION[home_team]
+        )
+        response = requests.get(url=url)
+        response.raise_for_status()
+
+        page = PlayByPlayPage(html=html.fromstring(response.content))
+
+        return self.parser.parse_play_by_plays(
+            play_by_plays=page.play_by_play_table.rows,
+            away_team_name=page.away_team_name,
+            home_team_name=page.home_team_name,
+        )
 
     def players_advanced_season_totals(self, season_end_year, include_combined_values=False):
         url = '{BASE_URL}/leagues/NBA_{season_end_year}_advanced.html'.format(
--- tests/unit/client/test_play_by_play.py
+++ tests/unit/client/test_play_by_play.py
@@ -5,16 +5,17 @@ from requests import HTTPError, codes
 from basketball_reference_web_scraper.client import play_by_play
 from basketball_reference_web_scraper.data import Team
 from basketball_reference_web_scraper.errors import InvalidDate
+from basketball_reference_web_scraper.http_service import HTTPService
 
 
 class TestPlayByPlay(TestCase):
-    @mock.patch("basketball_reference_web_scraper.client.http_client")
-    def test_raises_invalid_date_for_404_response(self, mocked_http_client):
-        mocked_http_client.play_by_play.side_effect = HTTPError(response=mock.Mock(status_code=codes.not_found))
+    @mock.patch.object(HTTPService, "play_by_play")
+    def test_raises_invalid_date_for_404_response(self, mocked_play_by_play):
+        mocked_play_by_play.side_effect = HTTPError(response=mock.Mock(status_code=codes.not_found))
         self.assertRaises(InvalidDate, play_by_play, home_team=Team.MILWAUKEE_BUCKS,  day=1, month=1, year=2018)
 
-    @mock.patch("basketball_reference_web_scraper.client.http_client")
-    def test_raises_non_404_http_error(self, mocked_http_client):
-        mocked_http_client.play_by_play.side_effect = HTTPError(response=mock.Mock(status_code=codes.server_error))
+    @mock.patch.object(HTTPService, "play_by_play")
+    def test_raises_non_404_http_error(self, mocked_play_by_play):
+        mocked_play_by_play.side_effect = HTTPError(response=mock.Mock(status_code=codes.server_error))
         self.assertRaises(HTTPError, play_by_play, home_team=Team.MILWAUKEE_BUCKS,  day=1, month=1, year=2018)
 
