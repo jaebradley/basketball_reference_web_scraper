e9230dd678ea3bd8cb3a547a0f0d57a33e57c196
Jae Bradley <jae.b.bradley@gmail.com>
2020-04-27 17:37:30 -0400

    Migrate team box scores to http service and parser service (#178)

--- basketball_reference_web_scraper/http_client.py
+++ basketball_reference_web_scraper/http_client.py
@@ -1,55 +1,14 @@
 import requests
 from lxml import html
 
-from basketball_reference_web_scraper.data import TEAM_ABBREVIATIONS_TO_TEAM, TeamTotal, \
-    LEAGUE_ABBREVIATIONS_TO_LEAGUE, PlayerData
-from basketball_reference_web_scraper.html import BoxScoresPage, \
-    DailyBoxScoresPage, SearchPage, PlayerPage
-from basketball_reference_web_scraper.parsers import TeamAbbreviationParser, \
-    TeamTotalsParser, SearchResultNameParser, \
+from basketball_reference_web_scraper.data import LEAGUE_ABBREVIATIONS_TO_LEAGUE, PlayerData
+from basketball_reference_web_scraper.html import SearchPage, PlayerPage
+from basketball_reference_web_scraper.parsers import SearchResultNameParser, \
     ResourceLocationParser, SearchResultsParser, LeagueAbbreviationParser, PlayerDataParser
 
 BASE_URL = 'https://www.basketball-reference.com'
 PLAY_BY_PLAY_TIMESTAMP_FORMAT = "%M:%S.%f"
 SEARCH_RESULT_RESOURCE_LOCATION_REGEX = '(https?:\/\/www\.basketball-reference\.com\/)?(?P<resource_type>.+?(?=\/)).*\/(?P<resource_identifier>.+).html'
-
-
-def team_box_score(game_url_path):
-    url = "{BASE_URL}/{game_url_path}".format(BASE_URL=BASE_URL, game_url_path=game_url_path)
-
-    response = requests.get(url=url)
-
-    response.raise_for_status()
-
-    page = BoxScoresPage(html.fromstring(response.content))
-    combined_team_totals = [
-        TeamTotal(team_abbreviation=table.team_abbreviation, totals=table.team_totals)
-        for table in page.basic_statistics_tables
-    ]
-    parser = TeamTotalsParser(team_abbreviation_parser=TeamAbbreviationParser(
-        abbreviations_to_teams=TEAM_ABBREVIATIONS_TO_TEAM,
-    ))
-
-    return parser.parse(
-        first_team_totals=combined_team_totals[0],
-        second_team_totals=combined_team_totals[1],
-    )
-
-
-def team_box_scores(day, month, year):
-    url = "{BASE_URL}/boxscores/".format(BASE_URL=BASE_URL)
-
-    response = requests.get(url=url, params={"day": day, "month": month, "year": year})
-
-    response.raise_for_status()
-
-    page = DailyBoxScoresPage(html=html.fromstring(response.content))
-
-    return [
-        box_score
-        for game_url_path in page.game_url_paths
-        for box_score in team_box_score(game_url_path=game_url_path)
-    ]
 
 
 def search(term):
