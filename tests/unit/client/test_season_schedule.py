7829bc584ce1ab7dc80471cb9998451823dc9585
Jae Bradley <jae.b.bradley@gmail.com>
2020-04-27 16:57:15 -0400

    Fix unit tests for season schedule

--- tests/unit/client/test_season_schedule.py
+++ tests/unit/client/test_season_schedule.py
@@ -5,10 +5,11 @@ from requests import HTTPError, codes
 
 from basketball_reference_web_scraper.client import season_schedule
 from basketball_reference_web_scraper.errors import InvalidSeason
+from basketball_reference_web_scraper.http_service import HTTPService
 
 
 class TestSeasonSchedule(TestCase):
-    @patch("basketball_reference_web_scraper.http_client.season_schedule")
+    @patch.object(HTTPService, "season_schedule")
     def test_not_found_raises_invalid_season(self, mocked_season_schedule):
         mocked_season_schedule.side_effect = HTTPError(response=MagicMock(status_code=codes.not_found))
         self.assertRaisesRegex(
@@ -17,7 +18,7 @@ class TestSeasonSchedule(TestCase):
             season_schedule,
             season_end_year="jaebaebae")
 
-    @patch("basketball_reference_web_scraper.http_client.season_schedule")
+    @patch.object(HTTPService, "season_schedule")
     def test_other_http_error_is_raised(self, mocked_season_schedule):
         mocked_season_schedule.side_effect = HTTPError(response=MagicMock(status_code=codes.internal_server_error))
         self.assertRaises(HTTPError, season_schedule, season_end_year=2018)
