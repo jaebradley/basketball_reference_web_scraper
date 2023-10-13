8ab06103b8baa00aefc34c1c744f9bd1687dc8d2
Jae Bradley <jae.b.bradley@gmail.com>
2023-10-13 14:17:59 -0400

    Remove object creation duplication

--- basketball_reference_web_scraper/client.py
+++ basketball_reference_web_scraper/client.py
@@ -8,15 +8,19 @@ from basketball_reference_web_scraper.output.columns import BOX_SCORE_COLUMN_NAM
     PLAYER_SEASON_BOX_SCORE_COLUMN_NAMES, SEARCH_RESULTS_COLUMN_NAMES, STANDINGS_COLUMNS_NAMES
 from basketball_reference_web_scraper.output.fields import format_value, BasketballReferenceJSONEncoder
 from basketball_reference_web_scraper.output.service import OutputService
-from basketball_reference_web_scraper.output.writers import CSVWriter, JSONWriter, FileOptions, OutputOptions, \
-    SearchCSVWriter
+from basketball_reference_web_scraper.output.writers import CSVWriter, JSONWriter, FileOptions, OutputOptions
 from basketball_reference_web_scraper.parser_service import ParserService
+
+http_service = HTTPService(parser=ParserService())
+output_service = OutputService(
+    json_writer=JSONWriter(value_formatter=BasketballReferenceJSONEncoder),
+    csv_writer=CSVWriter(value_formatter=format_value)
+)
 
 
 def standings(season_end_year, output_type=None, output_file_path=None, output_write_option=None,
               json_options=None):
     try:
-        http_service = HTTPService(parser=ParserService())
         values = http_service.standings(season_end_year=season_end_year)
     except requests.exceptions.HTTPError as http_error:
         if http_error.response.status_code == requests.codes.not_found:
@@ -29,17 +33,12 @@ def standings(season_end_year, output_type=None, output_file_path=None, output_w
         json_options=json_options,
         csv_options={"column_names": STANDINGS_COLUMNS_NAMES}
     )
-    output_service = OutputService(
-        json_writer=JSONWriter(value_formatter=BasketballReferenceJSONEncoder),
-        csv_writer=CSVWriter(value_formatter=format_value)
-    )
     return output_service.output(data=values, options=options)
 
 
 def player_box_scores(day, month, year, output_type=None, output_file_path=None, output_write_option=None,
                       json_options=None):
     try:
-        http_service = HTTPService(parser=ParserService())
         values = http_service.player_box_scores(day=day, month=month, year=year)
     except requests.exceptions.HTTPError as http_error:
         if http_error.response.status_code == requests.codes.not_found:
@@ -53,17 +52,12 @@ def player_box_scores(day, month, year, output_type=None, output_file_path=None,
         json_options=json_options,
         csv_options={"column_names": BOX_SCORE_COLUMN_NAMES}
     )
-    output_service = OutputService(
-        json_writer=JSONWriter(value_formatter=BasketballReferenceJSONEncoder),
-        csv_writer=CSVWriter(value_formatter=format_value)
-    )
     return output_service.output(data=values, options=options)
 
 
 def regular_season_player_box_scores(player_identifier, season_end_year, output_type=None, output_file_path=None,
                                      output_write_option=None, json_options=None, include_inactive_games=False):
     try:
-        http_service = HTTPService(parser=ParserService())
         values = http_service.regular_season_player_box_scores(
             player_identifier=player_identifier,
             season_end_year=season_end_year,
@@ -81,17 +75,12 @@ def regular_season_player_box_scores(player_identifier, season_end_year, output_
         json_options=json_options,
         csv_options={"column_names": PLAYER_SEASON_BOX_SCORE_COLUMN_NAMES}
     )
-    output_service = OutputService(
-        json_writer=JSONWriter(value_formatter=BasketballReferenceJSONEncoder),
-        csv_writer=CSVWriter(value_formatter=format_value)
-    )
     return output_service.output(data=values, options=options)
 
 
 def playoff_player_box_scores(player_identifier, season_end_year, output_type=None, output_file_path=None,
                               output_write_option=None, json_options=None, include_inactive_games=False):
     try:
-        http_service = HTTPService(parser=ParserService())
         values = http_service.playoff_player_box_scores(
             player_identifier=player_identifier,
             season_end_year=season_end_year,
@@ -110,17 +99,12 @@ def playoff_player_box_scores(player_identifier, season_end_year, output_type=No
         json_options=json_options,
         csv_options={"column_names": PLAYER_SEASON_BOX_SCORE_COLUMN_NAMES}
     )
-    output_service = OutputService(
-        json_writer=JSONWriter(value_formatter=BasketballReferenceJSONEncoder),
-        csv_writer=CSVWriter(value_formatter=format_value)
-    )
     return output_service.output(data=values, options=options)
 
 
 def season_schedule(season_end_year, output_type=None, output_file_path=None, output_write_option=None,
                     json_options=None):
     try:
-        http_service = HTTPService(parser=ParserService())
         values = http_service.season_schedule(season_end_year=season_end_year)
     except requests.exceptions.HTTPError as http_error:
         # https://github.com/requests/requests/blob/master/requests/status_codes.py#L58
@@ -134,17 +118,12 @@ def season_schedule(season_end_year, output_type=None, output_file_path=None, ou
         json_options=json_options,
         csv_options={"column_names": SCHEDULE_COLUMN_NAMES}
     )
-    output_service = OutputService(
-        json_writer=JSONWriter(value_formatter=BasketballReferenceJSONEncoder),
-        csv_writer=CSVWriter(value_formatter=format_value)
-    )
     return output_service.output(data=values, options=options)
 
 
 def players_season_totals(season_end_year, output_type=None, output_file_path=None, output_write_option=None,
                           json_options=None):
     try:
-        http_service = HTTPService(parser=ParserService())
         values = http_service.players_season_totals(season_end_year=season_end_year)
     except requests.exceptions.HTTPError as http_error:
         if http_error.response.status_code == requests.codes.not_found:
@@ -157,17 +136,12 @@ def players_season_totals(season_end_year, output_type=None, output_file_path=No
         json_options=json_options,
         csv_options={"column_names": PLAYER_SEASON_TOTALS_COLUMN_NAMES}
     )
-    output_service = OutputService(
-        json_writer=JSONWriter(value_formatter=BasketballReferenceJSONEncoder),
-        csv_writer=CSVWriter(value_formatter=format_value)
-    )
     return output_service.output(data=values, options=options)
 
 
 def players_advanced_season_totals(season_end_year, include_combined_values=False, output_type=None,
                                    output_file_path=None, output_write_option=None, json_options=None):
     try:
-        http_service = HTTPService(parser=ParserService())
         values = http_service.players_advanced_season_totals(
             season_end_year,
             include_combined_values=include_combined_values
@@ -183,17 +157,12 @@ def players_advanced_season_totals(season_end_year, include_combined_values=Fals
         json_options=json_options,
         csv_options={"column_names": PLAYER_ADVANCED_SEASON_TOTALS_COLUMN_NAMES}
     )
-    output_service = OutputService(
-        json_writer=JSONWriter(value_formatter=BasketballReferenceJSONEncoder),
-        csv_writer=CSVWriter(value_formatter=format_value)
-    )
     return output_service.output(data=values, options=options)
 
 
 def team_box_scores(day, month, year, output_type=None, output_file_path=None, output_write_option=None,
                     json_options=None):
     try:
-        http_service = HTTPService(parser=ParserService())
         values = http_service.team_box_scores(day=day, month=month, year=year)
     except requests.exceptions.HTTPError as http_error:
         if http_error.response.status_code == requests.codes.not_found:
@@ -206,17 +175,12 @@ def team_box_scores(day, month, year, output_type=None, output_file_path=None, o
         json_options=json_options,
         csv_options={"column_names": TEAM_BOX_SCORES_COLUMN_NAMES}
     )
-    output_service = OutputService(
-        json_writer=JSONWriter(value_formatter=BasketballReferenceJSONEncoder),
-        csv_writer=CSVWriter(value_formatter=format_value)
-    )
     return output_service.output(data=values, options=options)
 
 
 def play_by_play(home_team, day, month, year, output_type=None, output_file_path=None, output_write_option=None,
                  json_options=None):
     try:
-        http_service = HTTPService(parser=ParserService())
         values = http_service.play_by_play(home_team=home_team, day=day, month=month, year=year)
     except requests.exceptions.HTTPError as http_error:
         if http_error.response.status_code == requests.codes.not_found:
@@ -229,24 +193,15 @@ def play_by_play(home_team, day, month, year, output_type=None, output_file_path
         json_options=json_options,
         csv_options={"column_names": PLAY_BY_PLAY_COLUMN_NAMES}
     )
-    output_service = OutputService(
-        json_writer=JSONWriter(value_formatter=BasketballReferenceJSONEncoder),
-        csv_writer=CSVWriter(value_formatter=format_value)
-    )
     return output_service.output(data=values, options=options)
 
 
 def search(term, output_type=None, output_file_path=None, output_write_option=None, json_options=None):
-    http_service = HTTPService(parser=ParserService())
     values = http_service.search(term=term)
     options = OutputOptions.of(
         file_options=FileOptions.of(path=output_file_path, mode=output_write_option),
         output_type=output_type,
         json_options=json_options,
         csv_options={"column_names": SEARCH_RESULTS_COLUMN_NAMES}
-    )
-    output_service = OutputService(
-        json_writer=JSONWriter(value_formatter=BasketballReferenceJSONEncoder),
-        csv_writer=SearchCSVWriter(value_formatter=format_value)
     )
     return output_service.output(data=values, options=options)
