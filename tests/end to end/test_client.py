4b222a5251e104f2db504434fd94fc06a01945d2
Jae Bradley <jae.b.bradley@gmail.com>
2024-03-27 22:00:58 -0400

    Add more end-to-end tests (#261)

--- tests/end to end/test_client.py
+++ tests/end to end/test_client.py
@@ -1,11 +1,19 @@
+import datetime
+import os
+import time
 from unittest import TestCase
 
-from basketball_reference_web_scraper import client
-from basketball_reference_web_scraper.data import Location, Team, Outcome
+import basketball_reference_web_scraper.client as client
+from basketball_reference_web_scraper.data import Location, Outcome
+from basketball_reference_web_scraper.data import OutputWriteOption, OutputType, Team, PeriodType
 
 
-class TestPlayerBoxScores(TestCase):
-    def test(self):
+class TestClient(TestCase):
+    def setUp(self):
+        # To avoid getting rate-limited
+        time.sleep(10)
+
+    def test_player_box_scores(self):
         box_scores = client.player_box_scores(day=11, month=3, year=2024)
         self.assertIsNotNone(box_scores)
         self.assertNotEqual(0, len(box_scores))
@@ -34,3 +42,67 @@ class TestPlayerBoxScores(TestCase):
             "game_score": 42.5,
         },
             box_scores[0])
+
+    def test_2001_season_schedule(self):
+        schedule = client.season_schedule(season_end_year=2001)
+        self.assertIsNotNone(schedule)
+
+    def test_current_year_season_schedule(self):
+        season_schedule = client.season_schedule(season_end_year=datetime.datetime.now().year)
+        self.assertIsNotNone(season_schedule)
+
+    def test_2019_player_advanced_season_totals(self):
+        player_season_totals = client.players_advanced_season_totals(season_end_year=2019)
+        self.assertIsNotNone(player_season_totals)
+        self.assertTrue(len(player_season_totals) > 0)
+
+    def test_BOS_2018_10_16_play_by_play(self):
+        play_by_play = client.play_by_play(
+            home_team=Team.BOSTON_CELTICS,
+            day=16,
+            month=10,
+            year=2018,
+        )
+        self.assertIsNotNone(play_by_play)
+
+    def test_BOS_2018_10_16_play_by_play_csv_to_file(self):
+        output_file_path = os.path.join(
+            os.path.dirname(__file__),
+            "./output/2018_10_16_BOS_pbp.csv",
+        )
+        client.play_by_play(
+            home_team=Team.BOSTON_CELTICS,
+            day=16,
+            month=10,
+            year=2018,
+            output_type=OutputType.CSV,
+            output_file_path=output_file_path,
+            output_write_option=OutputWriteOption.WRITE,
+        )
+
+    def test_overtime_play_by_play(self):
+        play_by_play = client.play_by_play(
+            home_team=Team.PORTLAND_TRAIL_BLAZERS,
+            day=22,
+            month=10,
+            year=2018,
+        )
+        last_play = play_by_play[-1]
+        self.assertIsNotNone(last_play)
+        self.assertEqual(1, last_play["period"])
+        self.assertEqual(PeriodType.OVERTIME, last_play["period_type"])
+
+    def test_overtime_play_by_play_to_json_file(self):
+        output_file_path = os.path.join(
+            os.path.dirname(__file__),
+            "./output/2018_10_22_POR_pbp.json",
+        )
+        client.play_by_play(
+            home_team=Team.PORTLAND_TRAIL_BLAZERS,
+            day=22,
+            month=10,
+            year=2018,
+            output_type=OutputType.JSON,
+            output_file_path=output_file_path,
+            output_write_option=OutputWriteOption.WRITE,
+        )
