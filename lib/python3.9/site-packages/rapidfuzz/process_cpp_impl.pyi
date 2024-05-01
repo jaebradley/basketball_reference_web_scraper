d2df9a38ba1383972e8c8f0475f84dcfe36a4490
Jae Bradley <jae.b.bradley@gmail.com>
2024-05-01 00:07:42 -0400

    Update test to fix build issues (#269)

---
+++ lib/python3.9/site-packages/rapidfuzz/process_cpp_impl.pyi
@@ -0,0 +1,129 @@
+from __future__ import annotations
+
+from typing import (
+    Any,
+    Callable,
+    Collection,
+    Generator,
+    Hashable,
+    Iterable,
+    Mapping,
+    Sequence,
+    TypeVar,
+    overload,
+)
+
+from rapidfuzz.fuzz import WRatio, ratio
+
+_StringType = Sequence[Hashable]
+_AnyStringType = TypeVar("_AnyStringType", bound=_StringType)
+_S1 = TypeVar("_S1")
+_S2 = TypeVar("_S2")
+_ResultType = int | float
+
+@overload
+def extractOne(
+    query: _S1,
+    choices: Iterable[_S2],
+    *,
+    scorer: Callable[..., _ResultType] = WRatio,
+    processor: Callable[..., _StringType] | None = None,
+    score_cutoff: _ResultType | None = None,
+    score_hint: _ResultType | None = None,
+    scorer_kwargs: dict[str, Any] | None = None,
+) -> tuple[_S2, _ResultType, int]: ...
+@overload
+def extractOne(
+    query: _S1,
+    choices: Mapping[Any, _S2],
+    *,
+    scorer: Callable[..., _ResultType] = WRatio,
+    processor: Callable[..., _StringType] | None = None,
+    score_cutoff: _ResultType | None = None,
+    score_hint: _ResultType | None = None,
+    scorer_kwargs: dict[str, Any] | None = None,
+) -> tuple[_S2, _ResultType, Any]: ...
+@overload
+def extract(
+    query: _S1,
+    choices: Collection[_S2],
+    *,
+    scorer: Callable[..., _ResultType] = WRatio,
+    processor: Callable[..., _StringType] | None = None,
+    limit: int | None = 5,
+    score_cutoff: _ResultType | None = None,
+    score_hint: _ResultType | None = None,
+    scorer_kwargs: dict[str, Any] | None = None,
+) -> list[tuple[_S2, _ResultType, int]]: ...
+@overload
+def extract(
+    query: _S1,
+    choices: Mapping[Any, _S2],
+    *,
+    scorer: Callable[..., _ResultType] = WRatio,
+    processor: Callable[..., _StringType] | None = None,
+    limit: int | None = 5,
+    score_cutoff: _ResultType | None = None,
+    score_hint: _ResultType | None = None,
+    scorer_kwargs: dict[str, Any] | None = None,
+) -> list[tuple[_S2, _ResultType, Any]]: ...
+@overload
+def extract_iter(
+    query: _S1,
+    choices: Iterable[_S2],
+    *,
+    scorer: Callable[..., _ResultType] = WRatio,
+    processor: Callable[..., _StringType] | None = None,
+    score_cutoff: _ResultType | None = None,
+    score_hint: _ResultType | None = None,
+    scorer_kwargs: dict[str, Any] | None = None,
+) -> Generator[tuple[_S2, _ResultType, int], None, None]: ...
+@overload
+def extract_iter(
+    query: _S1,
+    choices: Mapping[Any, _S2],
+    *,
+    scorer: Callable[..., _ResultType] = WRatio,
+    processor: Callable[..., _StringType] | None = None,
+    score_cutoff: _ResultType | None = None,
+    score_hint: _ResultType | None = None,
+    scorer_kwargs: dict[str, Any] | None = None,
+) -> Generator[tuple[_S2, _ResultType, Any], None, None]: ...
+
+FLOAT32: int
+FLOAT64: int
+INT8: int
+INT16: int
+INT32: int
+INT64: int
+UINT8: int
+UINT16: int
+UINT32: int
+UINT64: int
+
+def cdist(
+    queries: Iterable[_S1],
+    choices: Iterable[_S2],
+    *,
+    scorer: Callable[..., _ResultType] = ratio,
+    processor: Callable[..., _StringType] | None = None,
+    score_cutoff: _ResultType | None = None,
+    score_hint: _ResultType | None = None,
+    score_multiplier: _ResultType | None = None,
+    dtype: int | None = None,
+    workers: int = 1,
+    scorer_kwargs: dict[str, Any] | None = None,
+) -> Any: ...
+def cpdist(
+    queries: Iterable[_S1],
+    choices: Iterable[_S2],
+    *,
+    scorer: Callable[..., _ResultType] = ratio,
+    processor: Callable[..., _StringType] | None = None,
+    score_cutoff: _ResultType | None = None,
+    score_hint: _ResultType | None = None,
+    score_multiplier: _ResultType | None = None,
+    dtype: int | None = None,
+    workers: int = 1,
+    scorer_kwargs: dict[str, Any] | None = None,
+) -> Any: ...
