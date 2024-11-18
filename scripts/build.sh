1b6445b09b5d6b728c7f87d4de2dabdf0867ebe1
Jae Bradley <jae.b.bradley@gmail.com>
2024-11-18 16:21:00 -0500

    Update

--- scripts/build.sh
+++ scripts/build.sh
@@ -39,7 +39,15 @@ function main() {
   if [[ "0" != "$?" ]]; then printf "Cannot execute poetry at ${poetry_program_path}\n" && exit 255; fi
 
   # TODO: @jaebradley re-enable integration pytests
-  "${poetry_program_path}" run coverage run --source=basketball_reference_web_scraper --module pytest --ignore "./tests/integration"
+  "${poetry_program_path}" run coverage run --source=basketball_reference_web_scraper --module pytest \
+    --ignore "./tests/integration/test_player_box_scores.py" \
+    --ignore "./tests/integration/test_player_season_totals.py" \
+    --ignore "./tests/integration/test_players_advanced_season_totals.py" \
+    --ignore "./tests/integration/test_players_season_totals.py" \
+    --ignore "./tests/integration/test_search.py" \
+    --ignore "./tests/integration/test_season_schedule.py" \
+    --ignore "./tests/integration/test_standings.py" \
+    --ignore "./tests/integration/test_team_box_scores.py" \
   local poetry_exit_code="$?"
   # https://docs.pytest.org/en/7.1.x/reference/exit-codes.html#:~:text=Exit%20code%205,No%20tests%20were%20collected&text=If%20you%20would%20like%20to,using%20the%20pytest%2Dcustom_exit_code%20plugin.
   if [[ "5" == "${poetry_exit_code}" ]]; then printf "pytest using poetry program at ${poetry_program_path} did not collect any tests" && exit 0; fi
--- tests/integration/client/test_play_by_play.py
+++ tests/integration/client/test_play_by_play.py
@@ -2,10 +2,77 @@ import json
 import os
 from unittest import TestCase
 
+import requests_mock
+
 from basketball_reference_web_scraper.client import play_by_play
 from basketball_reference_web_scraper.data import OutputType, OutputWriteOption, PeriodType
 from basketball_reference_web_scraper.data import Team
 from basketball_reference_web_scraper.errors import InvalidDate
+
+
+class Test199911160ATLPlayByPlay(TestCase):
+    def setUp(self):
+        with open(os.path.join(
+                os.path.dirname(__file__),
+                "../files/199911160ATL.html",
+        ), 'r') as file_input: self._html = file_input.read();
+
+    @requests_mock.Mocker()
+    def test_length(self, m):
+        m.get("https://www.basketball-reference.com/boxscores/pbp/199111160ATL.html", text=self._html, status_code=200)
+        plays = play_by_play(home_team=Team.ATLANTA_HAWKS, day=16, month=11, year=1991)
+        self.assertEqual(len(plays), 420)
+
+
+class Test201810270MILPlayByPlay(TestCase):
+    def setUp(self):
+        with open(os.path.join(
+                os.path.dirname(__file__),
+                "../files/201810270MIL.html",
+        ), 'r') as file_input: self._html = file_input.read();
+
+    @requests_mock.Mocker()
+    def test_length(self, m):
+        m.get('https://www.basketball-reference.com/boxscores/pbp/201810270MIL.html', text=self._html, status_code=200)
+        plays = play_by_play(home_team=Team.MILWAUKEE_BUCKS, day=27, month=10, year=2018)
+        self.assertEqual(len(plays), 465)
+
+    @requests_mock.Mocker()
+    def test_first_play(self, m):
+        m.get('https://www.basketball-reference.com/boxscores/pbp/201810270MIL.html', text=self._html, status_code=200)
+        plays = play_by_play(home_team=Team.MILWAUKEE_BUCKS, day=27, month=10, year=2018)
+        self.assertEqual(
+            plays[0],
+            {
+                "period": 1,
+                "period_type": PeriodType.QUARTER,
+                "relevant_team": Team.ORLANDO_MAGIC,
+                "away_team": Team.ORLANDO_MAGIC,
+                "home_team": Team.MILWAUKEE_BUCKS,
+                "away_score": 0,
+                "home_score": 0,
+                "description": "N. Vučević misses 2-pt hook shot from 3 ft",
+                "remaining_seconds_in_period": 703.0,
+            }
+        )
+
+    @requests_mock.Mocker()
+    def test_last_play(self, m):
+        m.get('https://www.basketball-reference.com/boxscores/pbp/201810290MIL.html', text=self._html, status_code=200)
+        plays = play_by_play(home_team=Team.MILWAUKEE_BUCKS, day=29, month=10, year=2018)
+        self.assertEqual(
+            plays[464],
+            {
+                "period": 4,
+                "period_type": PeriodType.QUARTER,
+                "relevant_team": Team.MILWAUKEE_BUCKS,
+                "away_team": Team.ORLANDO_MAGIC,
+                "home_team": Team.MILWAUKEE_BUCKS,
+                "away_score": 91,
+                "home_score": 113,
+                "description": "Defensive rebound by T. Maker",
+                "remaining_seconds_in_period": 2.0,
+            })
 
 
 class TestPlayByPlayInMemory(TestCase):
