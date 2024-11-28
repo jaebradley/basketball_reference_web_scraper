d5445f8a14006470ecc43593aeba17860e513607
Jae Bradley <jae.b.bradley@gmail.com>
2024-11-27 22:13:54 -0500

    Add tests for players advanced season totals

--- tests/integration/client/test_players_advanced_season_totals.py
+++ tests/integration/client/test_players_advanced_season_totals.py
@@ -1,6 +1,6 @@
+import filecmp
 import json
 import os
-import unittest
 from datetime import date
 from unittest import TestCase
 
@@ -24,10 +24,7 @@ class Test2019(TestCase):
         result = players_advanced_season_totals(season_end_year=2019)
         self.assertEqual(len(result), 623)
 
-    # TODO: @jaebradley add tests for fields
 
-
-@unittest.skip("Temporarily skip")
 class BaseTestPlayerAdvancedSeasonTotalsCSVOutput(TestCase):
     @property
     def year(self):
@@ -38,19 +35,29 @@ class BaseTestPlayerAdvancedSeasonTotalsCSVOutput(TestCase):
         raise NotImplementedError
 
     def setUp(self):
+        with open(os.path.join(
+                os.path.dirname(__file__),
+                f"../files/player_advanced_season_totals/{self.year}.html",
+        ), 'r') as file_input: self._html = file_input.read()
+
         self.output_file_path = os.path.join(
             os.path.dirname(__file__),
-            "../output/player_advanced_season_totals_{year}.csv".format(year=self.year),
+            f"./output/generated/player_advanced_season_totals/{self.year}.csv",
         )
         self.expected_output_file_path = os.path.join(
             os.path.dirname(__file__),
-            "../output/expected/player_advanced_season_totals_{year}.csv".format(year=self.year),
+            f"./output/expected/player_advanced_season_totals/{self.year}.csv",
         )
 
     def tearDown(self):
         os.remove(self.output_file_path)
 
-    def assert_player_advanced_season_totals_csv(self):
+    @requests_mock.Mocker()
+    def assert_player_advanced_season_totals_csv(self, m):
+        m.get(f"https://www.basketball-reference.com/leagues/NBA_{self.year}_advanced.html",
+              text=self._html,
+              status_code=200)
+
         players_advanced_season_totals(
             season_end_year=self.year,
             output_type=OutputType.CSV,
@@ -58,15 +65,12 @@ class BaseTestPlayerAdvancedSeasonTotalsCSVOutput(TestCase):
             include_combined_values=self.include_combined_values,
         )
 
-        with open(self.output_file_path, "r", encoding="utf8") as output_file, \
-                open(self.expected_output_file_path, "r", encoding="utf8") as expected_output_file:
-            self.assertEqual(
-                output_file.readlines(),
-                expected_output_file.readlines(),
-            )
+        self.assertTrue(
+            filecmp.cmp(
+                self.output_file_path,
+                self.expected_output_file_path))
 
 
-@unittest.skip("Temporarily skip")
 class BaseTestPlayerAdvancedSeasonTotalsJSONOutput(TestCase):
     @property
     def year(self):
@@ -77,19 +81,29 @@ class BaseTestPlayerAdvancedSeasonTotalsJSONOutput(TestCase):
         raise NotImplementedError
 
     def setUp(self):
+        with open(os.path.join(
+                os.path.dirname(__file__),
+                f"../files/player_advanced_season_totals/{self.year}.html",
+        ), 'r') as file_input: self._html = file_input.read()
+
         self.output_file_path = os.path.join(
             os.path.dirname(__file__),
-            "../output/player_advanced_season_totals_{year}.json".format(year=self.year),
+            f"./output/generated/player_advanced_season_totals/{self.year}.json",
         )
         self.expected_output_file_path = os.path.join(
             os.path.dirname(__file__),
-            "../output/expected/player_advanced_season_totals_{year}.json".format(year=self.year),
+            f"./output/expected/player_advanced_season_totals/{self.year}.json",
         )
 
     def tearDown(self):
         os.remove(self.output_file_path)
 
-    def assert_player_advanced_season_totals_json(self):
+    @requests_mock.Mocker()
+    def assert_player_advanced_season_totals_json(self, m):
+        m.get(f"https://www.basketball-reference.com/leagues/NBA_{self.year}_advanced.html",
+              text=self._html,
+              status_code=200)
+
         players_advanced_season_totals(
             season_end_year=self.year,
             output_type=OutputType.JSON,
@@ -105,7 +119,6 @@ class BaseTestPlayerAdvancedSeasonTotalsJSONOutput(TestCase):
             )
 
 
-@unittest.skip("Temporarily skip")
 class Test2018PlayerAdvancedSeasonTotalsCSVOutput(BaseTestPlayerAdvancedSeasonTotalsCSVOutput):
     @property
     def year(self):
@@ -132,7 +145,6 @@ class Test2017PlayerAdvancedSeasonTotalsCSVOutput(BaseTestPlayerAdvancedSeasonTo
         self.assert_player_advanced_season_totals_csv()
 
 
-@unittest.skip("Temporarily skip")
 class Test2016PlayerAdvancedSeasonTotalsCSVOutput(BaseTestPlayerAdvancedSeasonTotalsCSVOutput):
     @property
     def year(self):
@@ -146,7 +158,6 @@ class Test2016PlayerAdvancedSeasonTotalsCSVOutput(BaseTestPlayerAdvancedSeasonTo
         self.assert_player_advanced_season_totals_csv()
 
 
-@unittest.skip("Temporarily skip")
 class Test2001PlayerAdvancedSeasonTotalsCSVOutput(BaseTestPlayerAdvancedSeasonTotalsCSVOutput):
     @property
     def year(self):
@@ -160,7 +171,6 @@ class Test2001PlayerAdvancedSeasonTotalsCSVOutput(BaseTestPlayerAdvancedSeasonTo
         self.assert_player_advanced_season_totals_csv()
 
 
-@unittest.skip("Temporarily skip")
 class Test2018PlayerAdvancedSeasonTotalsJSONOutput(BaseTestPlayerAdvancedSeasonTotalsJSONOutput):
     @property
     def year(self):
@@ -174,7 +184,6 @@ class Test2018PlayerAdvancedSeasonTotalsJSONOutput(BaseTestPlayerAdvancedSeasonT
         self.assert_player_advanced_season_totals_json()
 
 
-@unittest.skip("Temporarily skip")
 class Test2017PlayerAdvancedSeasonTotalsJSONOutput(BaseTestPlayerAdvancedSeasonTotalsJSONOutput):
     @property
     def year(self):
@@ -188,7 +197,6 @@ class Test2017PlayerAdvancedSeasonTotalsJSONOutput(BaseTestPlayerAdvancedSeasonT
         self.assert_player_advanced_season_totals_json()
 
 
-@unittest.skip("Temporarily skip")
 class Test2016PlayerAdvancedSeasonTotalsJSONOutput(BaseTestPlayerAdvancedSeasonTotalsJSONOutput):
     @property
     def year(self):
@@ -202,7 +210,6 @@ class Test2016PlayerAdvancedSeasonTotalsJSONOutput(BaseTestPlayerAdvancedSeasonT
         self.assert_player_advanced_season_totals_json()
 
 
-@unittest.skip("Temporarily skip")
 class Test2001PlayerAdvancedSeasonTotalsJSONOutput(BaseTestPlayerAdvancedSeasonTotalsJSONOutput):
     @property
     def year(self):
@@ -216,8 +223,14 @@ class Test2001PlayerAdvancedSeasonTotalsJSONOutput(BaseTestPlayerAdvancedSeasonT
         self.assert_player_advanced_season_totals_json()
 
 
-@unittest.skip("Temporarily skip")
+@requests_mock.Mocker()
 class TestPlayerAdvancedSeasonTotalsInMemoryOutput(TestCase):
+    def setUp(self):
+        with open(os.path.join(
+                os.path.dirname(__file__),
+                f"../files/player_advanced_season_totals/2018.html",
+        ), 'r') as file_input: self._html = file_input.read()
+
     def test_future_season_raises_invalid_season(self):
         current_year = date.today().year
         future_year = current_year + 10
@@ -225,11 +238,19 @@ class TestPlayerAdvancedSeasonTotalsInMemoryOutput(TestCase):
         self.assertRaisesRegex(InvalidSeason, expected_message, players_advanced_season_totals,
                                season_end_year=future_year)
 
-    def test_2018_players_advanced_season_totals_length(self):
+    def test_2018_players_advanced_season_totals_length(self, m):
+        m.get(f"https://www.basketball-reference.com/leagues/NBA_2018_advanced.html",
+              text=self._html,
+              status_code=200)
+
         result = players_advanced_season_totals(season_end_year=2018)
         self.assertEqual(len(result), 605)
 
-    def test_first_2018_players_advanced_season_totals_row(self):
+    def test_first_2018_players_advanced_season_totals_row(self, m):
+        m.get(f"https://www.basketball-reference.com/leagues/NBA_2018_advanced.html",
+              text=self._html,
+              status_code=200)
+
         result = players_advanced_season_totals(season_end_year=2018)
         self.assertEqual(
             result[0],
@@ -267,7 +288,11 @@ class TestPlayerAdvancedSeasonTotalsInMemoryOutput(TestCase):
             },
         )
 
-    def test_last_2018_players_advanced_season_totals_row(self):
+    def test_last_2018_players_advanced_season_totals_row(self, m):
+        m.get(f"https://www.basketball-reference.com/leagues/NBA_2018_advanced.html",
+              text=self._html,
+              status_code=200)
+
         result = players_advanced_season_totals(season_end_year=2018)
         self.assertEqual(
             result[604],
@@ -308,7 +333,7 @@ class TestPlayerAdvancedSeasonTotalsInMemoryOutput(TestCase):
     def test_players_advanced_season_totals_json(self):
         expected_output_file_path = os.path.join(
             os.path.dirname(__file__),
-            "../output/expected/player_advanced_season_totals_2018.json",
+            "./output/expected/player_advanced_season_totals/2018.json",
         )
         result = players_advanced_season_totals(season_end_year=2018, output_type=OutputType.JSON)
         with open(expected_output_file_path, "r", encoding="utf8") as expected_output:
