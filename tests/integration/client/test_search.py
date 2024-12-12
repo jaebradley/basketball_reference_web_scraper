f93187208fe152a77f39fd1c204ddf3323ae3838
Jae Bradley <jae.b.bradley@gmail.com>
2024-12-12 18:16:06 -0500

    Update

--- tests/integration/client/test_search.py
+++ tests/integration/client/test_search.py
@@ -5,7 +5,7 @@ from unittest import TestCase
 import requests_mock
 
 from basketball_reference_web_scraper import client
-from basketball_reference_web_scraper.data import League, OutputType, OutputWriteOption
+from basketball_reference_web_scraper.data import OutputType, OutputWriteOption
 
 
 @requests_mock.Mocker()
@@ -79,10 +79,11 @@ class TestJa(TestCase):
         results = client.search(term="ja")
         self.assertEqual(863, len(results["players"]))
         self.assertEqual({
-                    "name": "LeBron James",
-                    "identifier": "jamesle01",
-                    "leagues": set()
-                }, results["players"][0])
+            "name": "LeBron James",
+            "identifier": "jamesle01",
+            "leagues": set()
+        }, results["players"][0])
+
 
 @requests_mock.Mocker()
 class TestAlonzoMourning(TestCase):
@@ -110,84 +111,144 @@ class TestAlonzoMourning(TestCase):
         )
 
 
-class TestSearchInMemory(TestCase):
-    def test_search_ja(self):
-        results = client.search(term="ja")
-        self.assertLessEqual(498, len(results["players"]))
+@requests_mock.Mocker()
+class TestDominiqueWilkins(TestCase):
+    def setUp(self):
+        with open(os.path.join(
+                os.path.dirname(__file__),
+                "../files/search/Dominique Wilkins.html"
+        ), 'r') as file_input: self._html = file_input.read()
 
-    def test_search_alonzo_mourning(self):
-        results = client.search(term="Alonzo Mourning")
-        self.assertEqual(
-            [
-                {
-                    "name": "Alonzo Mourning",
-                    "identifier": "mournal01",
-                    "leagues": {League.NATIONAL_BASKETBALL_ASSOCIATION}
-                }
-            ],
-            results["players"]
-        )
-
-    def test_search_alonz(self):
-        results = client.search(term="Alonz")
-        self.assertGreaterEqual(6, len(results["players"]))
-
-    def test_search_dominique_wilkins(self):
+    def test_result(self, m):
+        m.get(f"https://www.basketball-reference.com/search/search.fcgi?search=Dominique+Wilkins",
+              text=self._html,
+              status_code=200)
         results = client.search(term="Dominique Wilkins")
         self.assertEqual(
             [
                 {
                     "name": "Dominique Wilkins",
                     "identifier": "wilkido01",
-                    "leagues": {League.NATIONAL_BASKETBALL_ASSOCIATION}
+                    "leagues": set()
                 }
             ],
             results["players"]
         )
 
-    def test_search_rick_barry(self):
+
+@requests_mock.Mocker()
+class TestRickBarry(TestCase):
+    def setUp(self):
+        with open(os.path.join(
+                os.path.dirname(__file__),
+                "../files/search/Rick Barry.html"
+        ), 'r') as file_input: self._html = file_input.read()
+
+    def test_result(self, m):
+        m.get(f"https://www.basketball-reference.com/search/search.fcgi?search=Rick+Barry",
+              text=self._html,
+              status_code=200)
         results = client.search(term="Rick Barry")
         self.assertEqual(
             [
                 {
                     "name": "Rick Barry",
                     "identifier": "barryri01",
-                    "leagues": {League.NATIONAL_BASKETBALL_ASSOCIATION, League.AMERICAN_BASKETBALL_ASSOCIATION}
+                    "leagues": set()
                 }
             ],
             results["players"]
         )
 
-    def test_search_jaebaebae(self):
+
+@requests_mock.Mocker()
+class TestJaebaebae(TestCase):
+    def setUp(self):
+        with open(os.path.join(
+                os.path.dirname(__file__),
+                "../files/search/jaebaebae.html"
+        ), 'r') as file_input: self._html = file_input.read()
+
+    def test_result(self, m):
+        m.get(f"https://www.basketball-reference.com/search/search.fcgi?search=jaebaebae",
+              text=self._html,
+              status_code=200)
         results = client.search(term="jaebaebae")
-        self.assertListEqual([], results["players"])
+        self.assertEqual(
+            [],
+            results["players"]
+        )
 
-    def test_search_results_key(self):
-        results = client.search(term="jaebaebae")
-        self.assertListEqual(list(results), ["players"])
 
-    def test_length_of_kobe_search_results(self):
-        results = client.search(term="kobe")
-        self.assertEqual(4, len(results["players"]))
+@requests_mock.Mocker()
+class TestKobeBryant(TestCase):
+    def setUp(self):
+        with open(os.path.join(
+                os.path.dirname(__file__),
+                "../files/search/kobe bryant.html"
+        ), 'r') as file_input: self._html = file_input.read()
 
-    def test_players_in_kobe_search_results(self):
-        results = client.search(term="kobe")
-        self.assertListEqual(
+    def test_result(self, m):
+        m.get(f"https://www.basketball-reference.com/search/search.fcgi?search=kobe+bryant",
+              text=self._html,
+              status_code=200)
+        results = client.search(term="kobe bryant")
+        self.assertEqual(
             [
                 {
                     "name": "Kobe Bryant",
                     "identifier": "bryanko01",
-                    "leagues": {League.NATIONAL_BASKETBALL_ASSOCIATION}
+                    "leagues": set()
+                }
+            ],
+            results["players"]
+        )
+
+
+@requests_mock.Mocker()
+class TestKobe(TestCase):
+    def setUp(self):
+        with open(os.path.join(
+                os.path.dirname(__file__),
+                "../files/search/kobe.html"
+        ), 'r') as file_input: self._html = file_input.read()
+
+    def test_result(self, m):
+        m.get(f"https://www.basketball-reference.com/search/search.fcgi?search=kobe",
+              text=self._html,
+              status_code=200)
+        results = client.search(term="kobe")
+        self.assertEqual(
+            [
+                {
+                    "name": "Kobe Bryant",
+                    "identifier": "bryanko01",
+                    "leagues": set()
                 },
                 {
                     "name": "Ruben Patterson",
                     "identifier": "patteru01",
-                    "leagues": {League.NATIONAL_BASKETBALL_ASSOCIATION}
+                    "leagues": set()
                 },
                 {
                     "name": "Dion Waiters",
                     "identifier": "waitedi01",
-                    "leagues": {League.NATIONAL_BASKETBALL_ASSOCIATION}
+                    "leagues": set()
+                },
+                {
+                    "name": "Austin Reaves",
+                    "identifier": "reaveau01",
+                    "leagues": set()
+                },
+                {
+                    "name": "Kobe Bufkin",
+                    "identifier": "bufkiko01",
+                    "leagues": set()
+                },
+                {
+                    "name": "Kobe Brown",
+                    "identifier": "brownko01",
+                    "leagues": set()
                 },
                 {
                     "name": "Oleksandr Kobets",
@@ -197,23 +258,6 @@ class TestSearchInMemory(TestCase):
             ],
             results["players"]
         )
-
-    def test_exact_search_result(self):
-        results = client.search(term="kobe bryant")
-        self.assertEqual(
-            [
-                {
-                    "name": "Kobe Bryant",
-                    "identifier": "bryanko01",
-                    "leagues": {League.NATIONAL_BASKETBALL_ASSOCIATION}
-                }
-            ],
-            results["players"]
-        )
-
-    def test_large_search_pagination(self):
-        results = client.search(term="a")
-        self.assertGreaterEqual(len(results["players"]), 960)
 
 
 class TestSearchJSONOutput(TestCase):
