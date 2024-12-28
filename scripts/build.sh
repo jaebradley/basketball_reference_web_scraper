26198dca33c6b46e884c90b9c4d8a336348d094c
Jae Bradley <jae.b.bradley@gmail.com>
2024-12-27 20:31:21 -0500

    Reenable integration test for player box scores parser

--- scripts/build.sh
+++ scripts/build.sh
@@ -41,7 +41,6 @@ function main() {
   # TODO: @jaebradley re-enable integration pytests
   "${poetry_program_path}" run coverage run --source=basketball_reference_web_scraper --module pytest \
     --ignore="tests/end to end/" \
-    --ignore="tests/integration/parsers/test_parse_player_box_scores.py" \
     --ignore="tests/integration/parsers/test_parse_schedule.py" \
     --ignore="tests/integration/parsers/test_parse_teams.py" \
 
