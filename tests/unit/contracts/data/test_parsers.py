cdc1bd8af9e495bc47b07823b7b229ebd19a19a1
Jae Bradley <jae.b.bradley@gmail.com>
2025-03-24 23:49:50 -0400

    Add dummy test

--- tests/unit/contracts/data/test_parsers.py
+++ tests/unit/contracts/data/test_parsers.py
@@ -1,9 +1,11 @@
+import hashlib
+import itertools
 from decimal import Decimal
 from unittest import TestCase
 
 from basketball_reference_web_scraper.contracts.data.models import Salary
 from basketball_reference_web_scraper.contracts.data.parsers import deserialize_season_start_year, deserialize_team, \
-    deserialize_guaranteed_salary
+    deserialize_guaranteed_salary, SalariesBySeasonParser
 from basketball_reference_web_scraper.data import Team
 
 
@@ -43,10 +45,38 @@ class TestDeserializingGuaranteedSalary(TestCase):
             contract_values_by_column_identifier={"remain_gtd": "$1,234,567"}
         )
 
+
 class TestSalariesBySeasonParser(TestCase):
     def test_unknown_column_name_raises_error(self):
         raise NotImplementedError()
+
     def test_season_start_year_deserialization_error_raises_error(self):
         raise NotImplementedError()
+
     def test_salary_deserialization_error_raises_error(self):
         raise NotImplementedError()
+
+    def test_valid_salaries_by_season(self):
+        def generate_salary():
+            for value in itertools.count(start=0, step=1):
+                yield Salary(amount=Decimal(value), currency="$")
+
+        salary_generator = generate_salary()
+        parser = SalariesBySeasonParser(
+            season_start_year_deserializer=lambda v: int.from_bytes(hashlib.md5(v.encode("utf-8")).digest()[:8],
+                                                                    'little', signed=True),
+            salary_deserializer=lambda v: None if v is None else next(salary_generator)
+        )
+        assert {6699318081062747564: Salary(amount=Decimal('0'), currency='$'),
+                -2012135647395072713: Salary(amount=Decimal('1'), currency='$')} == parser.parse(
+            contract_values_by_column_identifier={
+                "f": "bar",
+                "b": "jae",
+                "remain_gtd": "test"
+            },
+            column_names_by_identifier={
+                "f": "foo",
+                "b": "bar",
+                "remain_gtd": "guaranteed_money"
+            }
+        )
