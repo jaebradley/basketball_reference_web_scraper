d89aef111d36cebf85979624049567b022c40974
Digital Currensy Inc. <38707648+DigitalCurrensy@users.noreply.github.com>
2026-07-27 14:23:28 -0700

    Use a raw string for the search result regex (#340)

--- basketball_reference_web_scraper/parser_service.py
+++ basketball_reference_web_scraper/parser_service.py
@@ -15,7 +15,7 @@ from basketball_reference_web_scraper.team_season.parsers import RosterParser, P
 class ParserService:
     PLAY_BY_PLAY_TIMESTAMP_FORMAT = "%M:%S.%f"
     PLAY_BY_PLAY_SCORES_REGEX = "(?P<away_team_score>[0-9]+)-(?P<home_team_score>[0-9]+)"
-    SEARCH_RESULT_RESOURCE_LOCATION_REGEX = '(https?:\/\/www\.basketball-reference\.com\/)?(?P<resource_type>.+?(?=\/)).*\/(?P<resource_identifier>.+).html'
+    SEARCH_RESULT_RESOURCE_LOCATION_REGEX = r'(https?:\/\/www\.basketball-reference\.com\/)?(?P<resource_type>.+?(?=\/)).*\/(?P<resource_identifier>.+).html'
 
     def __init__(self):
         self.team_abbreviation_parser = TeamAbbreviationParser()
